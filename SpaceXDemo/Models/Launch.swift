74eb0186f3dee932bacb7c4ba6c705cbe4d0bdf0
e.vaganov <evaganov@outlook.com>
2022-02-22 00:43:14 +0500

    init commit

---
+++ SpaceXDemo/Models/Launch.swift
@@ -0,0 +1,8 @@
+//
+//  Launch.swift
+//  SpaceXDemo
+//
+//  Created by Evgeniy Vaganov on 21.02.2022.
+//
+import Foundation
+
---
+++ SpaceXDemo/SpaceXDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXDemoApp.swift
+//  SpaceXDemo
+//
+//  Created by Evgeniy Vaganov on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
